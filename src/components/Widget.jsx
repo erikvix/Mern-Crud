9b72dceff8544dca895f556020842b7a7297b34e
Erik Carvalho <erikvixx@gmail.com>
2023-10-22 15:57:31 -0300

    refactor: better code to widget

--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -3,38 +3,55 @@ import "./Widget.css";
 import { Link, useLocation } from "react-router-dom";
 import { MdDashboard, MdPerson } from "react-icons/md";
 
-const Widget = ({ title, children }) => {
-  const location = useLocation();
-
+const WidgetBox = ({type, text, pathName}) => {
+  return(
+    <div
+      className={`widget-${type} ${
+        location.pathname === `${pathName}` ? "active" : ""
+      }`}
+    >
+      {
+        type === "dashboard" ?
+        <MdDashboard
+        size="24"
+        color={location.pathname === `${pathName}` ? "#61dbfb" : "#fff"}
+      />
+      :
+      <MdPerson
+        size="24"
+        color={location.pathname === `${pathName}` ? "#61dbfb" : "#fff"}
+      /> 
+      }
+      <Link to={pathName} >{text}</Link>
+  </div>
+  )
+}
+const Widget = () => {
+  const widgets = [{
+    type: "dashboard",
+    text: "Dashboard",
+    pathName: "/dashboard"
+  },
+  {
+    type: "users",
+    text: "UserList",
+    pathName: "/" 
+  }
+];
   return (
     <section className="widget-container">
       <div className="widget-items">
-        <div className="widget-box">
-          <div
-            className={`widget-dashboard ${
-              location.pathname === "/dashboard" ? "active" : ""
-            }`}
-          >
-            <MdDashboard
-              size="24"
-              color={location.pathname === "/dashboard" ? "#61dbfb" : "#fff"}
-            />
-            <Link to="/dashboard">Dashboard</Link>
-          </div>
-        </div>
-        <div className="widget-box">
-          <div
-            className={`widget-users ${
-              location.pathname === "/" ? "active" : ""
-            }`}
-          >
-            <MdPerson
-              size="24"
-              color={location.pathname === "/" ? "#61dbfb" : "#fff"}
-            />
-            <Link to="/">Users List</Link>
-          </div>
-        </div>
+        {
+          widgets.map((widget) =>{
+            return(
+              <WidgetBox
+                type={widget.type}
+                text={widget.text}
+                pathName={widget.pathName}
+              ></WidgetBox>
+            )
+          })
+        }
       </div>
     </section>
   );
