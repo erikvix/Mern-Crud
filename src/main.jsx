82b5aa929cb31ad374b422da2449d68e22007a3f
Erik Carvalho <erikvixx@gmail.com>
2023-11-09 21:20:47 -0300

    miragejs changes

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,30 @@ createServer({
   models: {
     user: Model,
   },
+  seeds(server) {
+    server.create("user", {
+      id: 1,
+      firstname: "Mikael",
+      lastname: "Hadler",
+      email: "mikaelhadler@gmail.com",
+      password: "123",
+    });
+    server.create("user", {
+      id: 2,
+      firstname: "Junior",
+      lastname: "Silva",
+      email: "juniorislva@gmail.com",
+      password: "12344",
+    });
+    server.create("user", {
+      id: 3,
+      firstname: "Pedro",
+      lastname: "Felix",
+      email: "pedrofelix@gmail.com",
+      password: "12321",
+    });
+  },
+
   routes() {
     this.urlPrefix = "https://crud-mernstack.netlify.app";
     this.get("/api/users", (schema) => {
