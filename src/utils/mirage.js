4b240e9ce30ed8d88ebe14644feca34758ddda3a
Erik Carvalho <erikvixx@gmail.com>
2023-11-15 19:45:47 -0300

    refactor: include url based on environment

--- src/utils/mirage.js
+++ src/utils/mirage.js
@@ -1,21 +1,33 @@
 import { createServer, Model } from "miragejs";
 import { mocks } from "@/utils/mocks";
 
-const initMirage = () => createServer({
+const initMirage = () =>
+  createServer({
     models: {
       user: Model,
     },
     seeds(server) {
-      mocks.seeds(server);    
+      mocks.seeds(server);
     },
     routes() {
-      this.urlPrefix = "https://crud-mernstack.netlify.app";
+      this.urlPrefix =
+        import.meta.env.MODE === "development"
+          ? "http://localhost:5173"
+          : "https://crud-mernstack.netlify.app";
       this.get("/api/users", (schema) => schema.users.all());
-      this.delete("/api/users/:id", (schema, request) => schema.users.find(request.params.id).destroy());
-      this.post("/api/user", (schema, request) => schema.user.create("user", JSON.parse(request.requestBody)));
-      this.put("/api/user/:id", (schema, request) => schema.user.find(request.params.id).update(JSON.parse(request.requestBody)));
+      this.delete("/api/users/:id", (schema, request) =>
+        schema.users.find(request.params.id).destroy()
+      );
+      this.post("/api/user", (schema, request) =>
+        schema.user.create("user", JSON.parse(request.requestBody))
+      );
+      this.put("/api/user/:id", (schema, request) =>
+        schema.user
+          .find(request.params.id)
+          .update(JSON.parse(request.requestBody))
+      );
       this.passthrough("https://unpkg.com/**/*");
     },
-});
+  });
 
 export { initMirage };
