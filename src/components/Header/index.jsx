e8ecb3925766040ed26545bfcb6c4e3b69c5e4d6
erikvix <erikvixx@gmail.com>
2023-12-29 10:05:09 -0300

    small changes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -13,19 +13,17 @@ const Header = () => {
 
   return (
     <header className="flex items-center px-4 py-8 justify-between md:flex md:justify-end">
-      <IoMenu
-        className="flex md:hidden cursor-pointer"
-        onClick={handleOpenSidebar}
-      />
+      <div className="flex items-center gap-2">
+        <IoMenu
+          className="flex md:hidden cursor-pointer"
+          onClick={handleOpenSidebar}
+        />
+        <Logo className={"w-10"} />
+        <span>React App</span>
+      </div>
       {isOpen && (
         <aside className="fixed top-20 md:flex flex-col bg-[#0f172a] w-48">
-          <div className="flex flex-col items-center my-8 md:mt-12">
-            <Logo />
-            <p className="mt-4 ">
-              React <span>App</span>
-            </p>
-          </div>
-          <div className="p-4">
+          <div className="mt-10 p-4">
             <NavLink />
           </div>
         </aside>
--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,6 +1,8 @@
 import logo from "@/assets/react.svg";
 import "@/components/Logo/Logo.css";
 
-const Logo = () => <img className="logo" src={logo} alt="" />;
+const Logo = ({ className = "logo" }) => (
+  <img className={className} src={logo} alt="" />
+);
 
 export default Logo;
