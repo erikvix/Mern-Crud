18859f6ea2d6cd5ddf5c23cfc76194bbcd2088d4
Erik Nunes <erikvixx@gmail.com>
2024-02-23 09:13:05 -0300

    test url

--- src/utils/mirage.js
+++ src/utils/mirage.js
@@ -33,9 +33,7 @@ const initMirage = () =>
         schema.users.find(request.params.id).update(body);
       });
       this.passthrough("https://unpkg.com/**/*");
-
       this.passthrough("https://crud-mernstack.netlify.app/**/*");
-
       this.passthrough("https://crud-mernstack.netlify.app/*");
       this.passthrough("http://crud-mernstack.netlify.app/*");
       this.passthrough();
