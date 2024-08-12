6f6f36ae804e3a749008998c512ac44a0e287a56
Jusu <erikvixx@gmail.com>
2024-08-12 10:47:53 -0300

    feat: error test

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import Button from "@/components/Button";
 
 export default function Settings() {
   return (
     <div className="">
       <div>
         <h3>Settings</h3>
+        <Button
+          onClick={() => {
+            throw new Error("Not Implemented");
+          }}
+        >
+          throw error
+        </Button>
       </div>
     </div>
   );
