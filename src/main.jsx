db038a1e05a096846b3f630653c63531522de834
Jusu <erikvixx@gmail.com>
2024-08-12 10:36:38 -0300

    feat: added sentryio

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,27 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "@/routes";
 import { initMirage } from "@/utils/mirage";
 import netlifyIdentity from "netlify-identity-widget";
+import * as Sentry from "@sentry/react";
 
 import "boxicons";
 import "./index.css";
 netlifyIdentity.init();
 
 initMirage();
+
+Sentry.init({
+  dsn: "https://efd77908490027db97f2b5f163457c39@o4507685567463424.ingest.us.sentry.io/4507764216102912",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.browserProfilingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  profilesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "jusustudio",
+    project: "portal"
+  })],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+
+  build: {
+    sourcemap: true
   }
 })
